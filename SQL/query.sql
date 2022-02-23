df029fe235eeb1e767ae438ccf1eb9b4a95617e7
Codnay <62608401+Codnay@users.noreply.github.com>
2022-02-23 18:30:42 +0530

    Added basic flask implementation

--- SQL/query.sql
+++ SQL/query.sql
@@ -1,4 +1,4 @@
---Add specified length for fields that are known
+--Add specified length for fields that are known and various keys if necessary
 CREATE TABLE cities(cityId integer, city text, province text);
 CREATE TABLE managers(id integer, username text, password text, restId integer, lastRestId integer);
 CREATE TABLE persons(id integer,firstname text,lastname text,gender text, phone char(14), email text, username text , password text, dob char(10), priviledge integer);
@@ -13,4 +13,30 @@ begin
 update restaurants
 set restaurants.avgReview = ((restaurants.avgReview * restaurants.reviewCount) + I.stars)/restaurants.reviewCount + 1, restaurants.reviewCount = restaurants.reviewCount+1
 from inserted I 
-where I.restId = restaurants.restId
+where I.restId = restaurants.restId;
+
+-- BASIC QUERIES--
+
+--LOCATE--
+
+-- DECLARE @TestVariable AS VARCHAR(100)
+-- SET @TestVariable = 100.100
+
+--distance--
+-- select *
+-- from restaurants
+-- where 
+
+
+--review
+select * 
+from restaurants 
+
+
+
+
+--COMBINATION--
+
+
+
+
